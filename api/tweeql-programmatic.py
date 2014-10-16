d95c1d439ba3d60290b8f37992f169c46276cbbd
Albert Toledo <toledoalbert@gmail.com>
2014-10-15 20:24:44 -0400

    cleanup stuff, more bricksquad files removed

--- api/tweeql-programmatic.py
+++
@@ -1,6 +0,0 @@
-from tweeql.exceptions import TweeQLException
-from tweeql.query_runner import QueryRunner
-
-runner = QueryRunner()
-runner.run_query("SELECT text FROM twitter_sample;", False)
-#runner.stop_query()
