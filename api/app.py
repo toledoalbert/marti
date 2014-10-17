dcbb9e5163167fd0c82a4a3577d8f13ba7e17d42
Albert Toledo <toledoalbert@gmail.com>
2014-10-17 01:55:35 -0400

    req changed

--- api/app.py
+++ api/app.py
@@ -2,16 +2,19 @@ from flask import *
 import flask
 from flask.ext.tweepy import Tweepy
 from flask import jsonify
+import tweepy
 
 app = flask.Flask(__name__)
 
-app.config.setdefault('TWEEPY_CONSUMER_KEY', 'Lr5Rq8fUegY0HiZwQAklQfRDw')
-app.config.setdefault('TWEEPY_CONSUMER_SECRET', '5iFM3HOGTbuM8incfI1SNujNHnaoMALTyqKL7f25zmIJWWaOaa')
-app.config.setdefault('TWEEPY_ACCESS_TOKEN_KEY', '41089072-DUXKDDlEZNoA7mCfpQWGTVHcEEw4CNbPemJJs1ZpA')
-app.config.setdefault('TWEEPY_ACCESS_TOKEN_SECRET', 'o3SaoOZqvXG6kEeLIkt7YTBtv03iGLIDskj1SXk6RWWwv')
+# app.config.setdefault('TWEEPY_CONSUMER_KEY', 'Lr5Rq8fUegY0HiZwQAklQfRDw')
+# app.config.setdefault('TWEEPY_CONSUMER_SECRET', '5iFM3HOGTbuM8incfI1SNujNHnaoMALTyqKL7f25zmIJWWaOaa')
+# app.config.setdefault('TWEEPY_ACCESS_TOKEN_KEY', '41089072-DUXKDDlEZNoA7mCfpQWGTVHcEEw4CNbPemJJs1ZpA')
+# app.config.setdefault('TWEEPY_ACCESS_TOKEN_SECRET', 'o3SaoOZqvXG6kEeLIkt7YTBtv03iGLIDskj1SXk6RWWwv')
 
-tweepy = Tweepy(app)
+# tweepy = Tweepy(app)
 
+auth = tweepy.auth.OAuthHandler("Lr5Rq8fUegY0HiZwQAklQfRDw", "5iFM3HOGTbuM8incfI1SNujNHnaoMALTyqKL7f25zmIJWWaOaa", secure=True)
+api = tweepy.API(auth)
 
 @app.route("/")
 def Hello():
@@ -19,12 +22,8 @@ def Hello():
 
 @app.route("/timeline")
 def timeline():
-    api = tweepy.api
-    tweets = api.public_timeline()
-    for tweet in tweets:
+    for tweet in api.home_timeline():
         print tweet.text
-        
-    return "dasdasdasd"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
